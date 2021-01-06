7c041165ea74582c25a464804c0694f5509e6b90
Richard Wilkosz <richardwilkosz97@gmail.com>
2021-01-05 23:57:52 -0600

    Changed theme colors, Started work on app bar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,73 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+  <v-app dark>
+    <v-app-bar app>
+      <v-toolbar-items>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-autocomplete
+          solo
+          flat
+          placeholder="Search movies"
+          prepend-inner-icon="mdi-magnify"
+          class="pt-1"
+        ></v-autocomplete>
+      </v-toolbar-items>
     </v-app-bar>
 
+    <v-navigation-drawer v-model="drawer" absolute temporary app>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+        >
+          <v-list-item>
+            <v-list-item-title>Homepage</v-list-item-title>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+            <v-list-item-content>
+            <v-list-item-title>Browse genres</v-list-item-title>
+            </v-list-item-content>
+
+          <v-list-item>
+            <v-list-item-title>Romance</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Sci-Fi</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Comedy</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
   },
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
   }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
 };
 </script>
