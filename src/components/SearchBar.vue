288d53ba548984e9edfdd1e39b77a9a5a89a0712
Richard Wilkosz <richardwilkosz97@gmail.com>
2021-04-11 00:04:29 -0500

    Overhauled search bar behavior, back to debounced input listening

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,28 +1,45 @@
 <template>
-    <v-combobox
-      solo
-      flat
-      dense
-      clearable
-      id="searchBar"
-      class="mt-6"
-      placeholder="Search movies"
-      append-icon="mdi-magnify"
-      v-model="input"
-      :items="ownedMovieTitles"
-      @change="updateSearch(input)"
-      @click:append="endSearch(input)"
-      @keydown.enter="endSearch(input)"
-      @click:clear="clearSearch"
-    ></v-combobox>
+  <v-combobox
+    solo
+    flat
+    dense
+    clearable
+    id="searchBar"
+    class="mt-6"
+    placeholder="Search movies"
+    append-icon=""
+    :search-input.sync="searchInput"
+    :items="suggestedMovieTitles"
+    @keydown.enter="endSearch()"
+    @click:clear="clearSearch()"
+    @update:search-input="updateSearch()"
+  >
+    <!-- <template v-slot:append-outer>
+      <v-btn
+        depressed
+        height="38"
+        min-width="38"
+        width="46"
+        color="grey darken-3"
+        @click="endSearch()"
+      >
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </template> -->
+    <template v-slot:prepend-item>
+      <v-subheader>SUGGESTED SEARCHES</v-subheader>
+    </template>
+  </v-combobox>
 </template>
 
 <script>
-// import _ from "lodash";
+import _ from "lodash";
 
 export default {
   data: () => ({
-    input: "",
+    // input: "",
+    searchInput: "",
+    suggestedMovieTitles: new Array(),
   }),
 
   props: {
@@ -30,16 +47,15 @@ export default {
   },
 
   methods: {
-    // getSuggestedSearches: function (input)
-
-    updateSearch: function (input) {
-      this.$emit("update-search", input ? input : "*");
+    updateSearch: function() {
+      // Only show suggestions when there's actual input
+      this.suggestedMovieTitles = this.searchInput ? this.ownedMovieTitles : new Array();
+      this.emitSearch();
     },
 
-    // Commented out instead of deleted as an example if debouncing ends up used elsewhere
-    // updateSearch: _.debounce(function (input) {
-    //   this.$emit("update-search", input ? input : "*");
-    // }, 500),
+    emitSearch: _.debounce(function () {
+      this.$emit("update-search", this.searchInput ? this.searchInput : "*");
+    }, 750),
 
     // Workaround since comboboxes don't close on enter
     endSearch: function () {
@@ -53,8 +69,12 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #searchBar ~ .v-input__append-inner .v-input__icon--append .v-icon {
   transform: none;
+}
+
+.v-input__append-outer {
+  margin: 0 !important;
 }
 </style>
