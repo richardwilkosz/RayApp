5ecd83bbfd40796dabafc1094177e771fe713563
Richard Wilkosz <richardwilkosz97@gmail.com>
2021-04-24 22:50:28 -0500

    Moved RayApp into its own folder/subcomponent of App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,215 +1,23 @@
 <template>
   <v-app dark>
-    <AppBar
-      @update-search="search"
-      @update-sort="updateSortBy"
-      @update-filter="updateFilterGenres"
-      @menu-view-all="queryAllOwned"
-      :genres="genres"
-      :ownedMovieTitles="getOwnedMovieTitles"
-    />
-    <v-main>
-      <v-container fluid>
-        <v-row>
-          <v-col lg="2" class="d-none d-lg-flex">
-            <SortFilterMenu
-              :genres="genres"
-              @update-sort="updateSortBy"
-              @update-filter="updateFilterGenres"
-            />
-          </v-col>
-          <v-col cols="12" lg="10">
-            <MovieList
-              :ownedMovies="sortFilterResults(ownedResults)"
-              :unownedMovies="sortFilterResults(unownedResults)"
-              :genres="genres"
-              :isLoading="isLoading"
-              :isSortingByYear="isSortingByYear"
-            />
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-    <AppFooter />
+    <RayApp></RayApp>
   </v-app>
 </template>
 
 <script>
-import axios from "axios";
-import Constants from "./assets/Constants.js";
-import SortService from "./services/SortService.js";
-import FilterService from "./services/FilterService.js";
+import RayApp from "./components/Ray/RayApp";
 
-import AppBar from "./components/AppBar";
-import SortFilterMenu from "./components/SortFilterMenu";
-import MovieList from "./components/MovieList";
-import AppFooter from "./components/AppFooter";
-
-export default {
-  name: "Ray",
-
-  data: () => ({
-    // Search
-    searchInput: "",
-    isLoading: true,
-
-    // API consumption
-    ownedMovies: [],
-    ownedResults: [],
-    unownedResults: [],
-
-    // Sort/filter
-    sortBy: Constants.SORT_ALPHA,
-    isSortingByYear: false,
-    filterGenreIds: [Constants.FILTER_DEFAULT],
-    genres: [],
-  }),
-
+export default { 
   components: {
-    AppBar,
-    SortFilterMenu,
-    MovieList,
-    AppFooter,
+    RayApp,
   },
-
-  created() {
-    let vm = this;
-    vm.$vuetify.theme.dark = true;
-
-    // Get list of owned movies
-    axios.get(Constants.OWNED_LIST_QUERY).then((response) => {
-      let ownedResults = response.data.items;
-      let ownedDetailPromises = [];
-
-      // Get extra details of each owned movie, e.g. runtime
-      ownedResults.forEach((result) =>
-        ownedDetailPromises.push(vm.getOwnedDetails(result.id))
-      );
-
-      // Get list of genres
-      axios.get(Constants.GENRES_QUERY).then((response) => {
-        vm.genres = response.data.genres;
-        vm.genres.unshift({
-          id: Constants.FILTER_DEFAULT,
-          name: "Include All Genres",
-        });
-      });
-
-      // Finalize list after API calls are complete
-      Promise.all(ownedDetailPromises).then(function (results) {
-        results.forEach((result) => vm.ownedMovies.push(result.data));
-        vm.queryAllOwned();
-        vm.isLoading = false;
-      });
-    });
-  },
-
-  computed: {
-    getOwnedMovieTitles() {
-      let titles = new Array();
-      this.ownedMovies.forEach((ownedMovie) => titles.push(ownedMovie.title));
-      return titles;
-    },
-  },
-
-  methods: {
-    /*
-      Search methods
-    */
-    search(searchInput) {
-      // Prevent duplicate searches
-      if (searchInput !== this.searchInput) {
-        this.searchInput = searchInput;
-
-        // Clear previous results
-        this.ownedResults = new Array();
-        this.unownedResults = new Array();
-
-        if (searchInput === Constants.SEARCH_ALL) {
-          this.queryAllOwned();
-        } else {
-          this.queryFromString(searchInput);
-        }
-      }
-    },
-
-    queryAllOwned() {
-      this.ownedResults = this.ownedMovies;
-    },
-
-    queryFromString(searchInput) {
-      let vm = this;
-      vm.isLoading = true;
-
-      // Query list of owned movies created at init
-      vm.ownedResults = vm.ownedMovies.filter((ownedMovie) =>
-        ownedMovie.title.toUpperCase().includes(searchInput.toUpperCase())
-      );
-
-      // Query API for unowned movies that match too, preventing duplicates
-      axios.get(Constants.SEARCH_QUERY + searchInput).then((response) => {
-        let allResults = response.data.results;
-        vm.unownedResults = allResults.filter(
-          (result) =>
-            !vm.ownedResults.find((ownedResult) => ownedResult.id === result.id)
-        );
-
-        vm.isLoading = false;
-      });
-    },
-
-    getOwnedDetails(id) {
-      return axios.get(Constants.DETAILS_QUERY(id));
-    },
-
-    /*
-      Sort/Filter methods
-    */
-    sortFilterResults: function (resultsArray) {
-      let sortedFilteredArray = resultsArray.slice(); // Copy by value, not by reference
-      sortedFilteredArray = this.filterResults(sortedFilteredArray);
-      this.sortResults(sortedFilteredArray);
-      return sortedFilteredArray;
-    },
-
-    updateSortBy(sortBy) {
-      this.sortBy = sortBy;
-
-      this.isSortingByYear =
-        this.sortBy === Constants.SORT_NEW ||
-        this.sortBy === Constants.SORT_OLD;
-    },
-
-    updateFilterGenres(filterGenreIds) {
-      this.filterGenreIds = filterGenreIds;
-    },
-
-    sortResults: function (resultsArray) {
-      return SortService.sortMovieArray(resultsArray, this.sortBy);
-    },
-
-    filterResults: function (resultsArray) {
-      return FilterService.filterMovieArray(resultsArray, this.filterGenreIds);
-    },
-  },
-};
+}
 </script>
 
-<style lang="scss">
-/* Custom font */
-@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap");
-$body-font-family: "Open Sans";
-$title-font: "Open Sans";
-.v-application {
-  font-family: $body-font-family, sans-serif !important;
-  .title {
-    // To pin point specific classes of some components
-    font-family: $title-font, sans-serif !important;
-  }
-}
-
-/* Used for mobile browsers' scroll bouncing */
-html {
-  background-color: #272727;
+<style lang="scss"></style>
+#AppWithFooter {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 </style>
